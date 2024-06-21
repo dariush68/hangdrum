84785119eda02abb888a82449d95e0c7cd2f7909
d.abedi <abedi.1989@gmail.com>
2024-06-21 07:00:49 +0330

    complete step 1

--- Core/static/Core/js/scripts.js
+++ Core/static/Core/js/scripts.js
@@ -1,4 +1,3 @@
-
 let currentNoteId = -1;
 let currentBur = 1;
 
@@ -142,6 +141,42 @@ function playNoteSequence(notes, durations, tempo) {
     playNextNote(); // Start the sequence
 }
 
+function playNoteSequenceJson(notes, tempo) {
+    let index = 0;
+    const baseDelay = (60 / tempo) * 1000; // Base delay for a crotchet (quarter note)
+
+    function playNextNote() {
+        if (index < notes.length) {
+            // const note = notes[index].trim().toUpperCase();
+            // const chord = notes[index].split(' ').map(note => note.trim().toUpperCase());
+            const chord = notes[index].cord;
+
+            const duration = notes[index].weight;
+            const delay = baseDelay * duration; // Adjust delay based on duration
+
+
+            chord.forEach(item => {
+                let noteMapping = item.note;
+                if(noteMapping === "N") return;
+                // console.log("noteMapping:"+noteMapping)
+                const url = noteList[`${noteMapping}`]
+                // console.log(url)
+                const audio = new Audio(`${url}`);
+                audio.play();
+            });
+
+            // const url = noteList[`${note}`]
+            // const audio = new Audio(`${url}`);
+            // audio.play();
+            index++;
+            setTimeout(playNextNote, delay); // Set timeout for the next note
+            // audio.onended = playNextNote; // Play the next note after the current one ends
+        }
+    }
+
+    playNextNote(); // Start the sequence
+}
+
 
 /*
 * Semibreve (whole note): 4
@@ -172,13 +207,13 @@ document.querySelectorAll('.section').forEach(section => {
 
 initSheet();
 
-function initSheet(){
+function initSheet() {
     $("#note-sheet").empty();
     console.log("empty sheet")
     addBar();
 }
 
-function addBar(){
+function addBar() {
 
     const barId = currentBur++;
 
@@ -188,11 +223,10 @@ function addBar(){
         <div id="note-bar-${barId}" class="row border mt-1"></div>
     `);
 
-    for (let i=1; i<=16; i++)
-    {
+    for (let i = 1; i <= 16; i++) {
         // console.log(i)
         let isBorder = "";
-        if(i%4===0) isBorder = "border-end";
+        if (i % 4 === 0) isBorder = "border-end";
 
         $(`#note-bar-${barId}`).append(`
             <div class="col ${isBorder}">
@@ -206,70 +240,129 @@ function addBar(){
     }
 }
 
-function play(){
+function play() {
     console.log("start play")
     console.log(currentBur)
 
     const notes = [];
     const durations = [];
 
-    for (let i=1; i<currentBur; i++){
+    let barIndex = 0;
 
-        let barIndex = 0;
-        for(let j=1; j<=16; j++){
-            //console.log(`note-bar-${i}-bit-${j}-1`)
-            const itemId = `note-bar-${i}-bit-${j}-1`;
-            const item = `#note-bar-${i}-bit-${j}-1`;
-            const val = $(item).text();
+    function processCord(i, j, cordId) {
+        //console.log(`note-bar-${i}-bit-${j}-1`)
+        const itemId = `note-bar-${i}-bit-${j}-${cordId}`;
+        const item = `#note-bar-${i}-bit-${j}-${cordId}`;
+        const val = $(item).text();
 
-            //-- check quit in first place
-            if(val === '-' && j === 1) {
-                let itms = getBarBitCord(itemId);
-                notes.push({
-                    "note": 'N',
-                    "bar": itms[0],
-                    "bit": itms[1],
-                    "cord": itms[2],
-                    "weight": 0.25,
-                    "hand": "R",
-                }); //-- 'N' is quit
-                barIndex++;
+        //-- check quit in first place
+        if (val === '-' && j === 1 && cordId === 1) {
+            let itms = getBarBitCord(itemId);
+            if(itms[0] === '1') return {
+                "note": 'N',
+                "bar": itms[0],
+                "bit": itms[1],
+                "cord": itms[2],
+                "weight": 4,
+                "hand": "R",
+            }; //-- 'N' is quit
+        }
+
+        if (val !== '-') {
+            //console.log(`${itemId}: ${val}`);
+            let itms = getBarBitCord(itemId);
+            return {
+                "note": val,
+                "bar": itms[0],
+                "bit": itms[1],
+                "cord": itms[2],
+                "weight": 4,
+                "hand": "R",
+            };
+        }
+
+        return null;
+    }
+
+    for (let i = 1; i < currentBur; i++) {
+
+        for (let j = 1; j <= 16; j++) {
+            let chord1 = processCord(i, j, 1)
+            let chord2 = processCord(i, j, 2)
+            let chord3 = processCord(i, j, 3)
+
+            /*if(chord1){console.log("chord1:")
+            console.log(chord1)}
+            if(chord2){console.log("chord2:")
+            console.log(chord2)}
+            if(chord3){console.log("chord3:")
+            console.log(chord3)}*/
+
+            let newNote = null;
+            if (chord1 !== null) {
+                newNote = {
+                    "bar": chord1.bar,
+                    "bit": chord1.bit,
+                    "weight": chord1.weight,
+                    "cord": [
+                        {
+                            "note": chord1.note,
+                            "hand": chord1.hand,
+                        }
+                    ]
+                };
             }
 
-            if(val !== '-'){
-               console.log(`${itemId}: ${val}`);
-                let itms = getBarBitCord(itemId);
-               notes.push({
-                    "note": val,
-                    "bar": itms[0],
-                    "bit": itms[1],
-                    "cord": itms[2],
-                    "weight": 0.25,
-                    "hand": "R",
-                });
+            if (chord2 !== null) {
+                newNote.cord.push({
+                    "note": chord2.note,
+                    "hand": chord2.hand,
+                })
+            }
 
-               if(barIndex > 0){
-                   console.log("prev:")
-                   console.log(notes[barIndex-1]);
-               }
+            if (chord3 !== null) {
+                newNote.cord.push({
+                    "note": chord3.note,
+                    "hand": chord3.hand,
+                })
+            }
 
+            if (newNote != null) {
 
-               barIndex++;
+                notes.push(newNote);
+                const currentBarIndex = barIndex++;
+
+                if (currentBarIndex > 0) {
+                    const prevNote = notes[currentBarIndex - 1];
+                    let diffBit = 0;
+                    //-- for same bar
+                    if(prevNote.bar === chord1.bar) diffBit = parseInt(chord1.bit) - parseInt(prevNote.bit);
+                    //-- for diff bar
+                    else{
+                        let prevDiffToEndBit = 16 - parseInt(prevNote.bit);
+                        let diffBar = (parseInt(chord1.bar) - parseInt(prevNote.bar) - 1) * 16;
+                        diffBit = parseInt(chord1.bit) + prevDiffToEndBit + diffBar;
+                    }
+
+                    notes[currentBarIndex - 1].weight = 0.25 * diffBit;
+                }
+
             }
 
         }
     }
 
     console.log(notes)
+    playNoteSequenceJson(notes, 120);
 }
 
-function getBarBitCord(itemId){
+function getBarBitCord(itemId) {
 
     let items = itemId.split('-');
     let bar = items[2]
     let bit = items[4]
     let cord = items[5]
 
-    return [bar,bit,cord]
+    return [bar, bit, cord]
 }
 
