c3aabf2ae89035940a67a0e3868eeda4ceec2216
dariush abedi <abedi.1989@gmail.com>
2024-06-22 07:58:53 +0330

    reform sheet list

--- Core/static/Core/js/sheetList.js
+++ Core/static/Core/js/sheetList.js
@@ -13,15 +13,17 @@ function GetSheetList(){
                 <div class="card mt-2">
                     <div class="card-body">
                         <div class="row">
-                            <div class="col-9">
+                            <div class="col-md-7 col-sm-12">
                                 <img src="${music_src}" alt="" height="50px"
                                      class="border rounded p-2">
                                 <span class="m-2">${data.results[i].title}</span>
-                                <span class="m-2 small"> <img src="${user_src}" alt="user" height="20px"> <span style="color: grey"> dariush abed</span></span>
+                                
         
                             </div>
-                            <div class="col-3 d-flex justify-content-end">
-                                <span class="mt-3 small"> <img src="${date_src}" alt="user" height="20px"> <span style="color: grey"> 2024-06-21</span></span>
+                            <div class="col-md-5 col-sm-12 d-flex justify-content-end">
+                                <span class="mt-3 small"> <img src="${user_src}" alt="user" height="20px" style="opacity: 0.4"> <span style="color: grey"> dariush abed</span></span>
+                                <span class="mt-3 small" style="margin-left: 30px"> <img src="${date_src}" alt="user" height="20px" style="opacity: 0.4"> <span style="color: grey"> 2024-06-21</span></span>
+                                <span class="mt-3 small" style="margin-left: 30px"> <img src="${eye_src}" alt="user" height="20px" style="opacity: 0.2"> <span style="color: grey" class="small"> 235</span></span>
                                 <a style="margin-left: 15px" href="#" title="Add sheet to List"><img src="${ticket_src}" alt="" height="50px"></a>
                                 <a href="#" title="Select sheet"><img src="${video_src}" alt="" height="50px"></a>
                             </div>
