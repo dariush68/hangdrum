136493fcd81e474ec260dec66fd1ea03e7e5b3e4
d.abedi <abedi.1989@gmail.com>
2025-02-20 20:02:15 +0330

    change base url

--- hangdrum/settings.py
+++ hangdrum/settings.py
@@ -157,11 +157,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATIC_ROOT = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    "/var/www/static/",
-]
+STATIC_ROOT = '/var/www/static/'
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+#     "/var/www/static/",
+# ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
